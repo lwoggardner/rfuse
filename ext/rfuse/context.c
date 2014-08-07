e584abb5059bc0534d6f0d84895f532bdb937474
lwoggardner <grant@lastweekend.com.au>
2014-08-07 19:47:25 +1000

    Allow filesystem methods to access the fuse object via Context

--- ext/rfuse/context.c
+++ ext/rfuse/context.c
@@ -10,7 +10,7 @@ VALUE wrap_context (struct fuse_context *fctx) {
 }
 
 /*
-   @api private
+   @private
 */
 VALUE context_initialize(VALUE self){
   return self;
@@ -25,7 +25,7 @@ VALUE context_new(VALUE class){
 }
 
 /*
-   @return [Integer] User id of the caller
+  @return [Integer] User id of the caller
 */
 VALUE context_uid(VALUE self){
   struct fuse_context *ctx;
@@ -52,12 +52,22 @@ VALUE context_pid(VALUE self){
 }
 
 /*
+ * @return [Fuse] the fuse object
+ */
+VALUE context_fuse(VALUE self) {
+  struct fuse_context *ctx;
+  Data_Get_Struct(self,struct fuse_context,ctx);
+  return (VALUE) ctx->private_data;
+}
+/*
    Document-class:  RFuse::Context
    Context object passed to every fuse operation
 */
 void context_init(VALUE module) {
 #if 0
     module = rb_define_module("RFuse");
+    
+
 #endif
 
   VALUE cContext=rb_define_class_under(module,"Context",rb_cObject);
@@ -66,4 +76,5 @@ void context_init(VALUE module) {
   rb_define_method(cContext,"uid",context_uid,0);
   rb_define_method(cContext,"gid",context_gid,0);
   rb_define_method(cContext,"pid",context_pid,0);
+  rb_define_method(cContext,"fuse",context_fuse,0);
 }
